25cc6444fa805277157489cb8fad8990438080ac
Daniel Nobrega <danielnobrega03@hotmail.com>
2022-10-28 21:15:22 -0300

    Adição da Função sendWeek()

--- src/main/java/fiap/java/UtilsBot.java
+++ src/main/java/fiap/java/UtilsBot.java
@@ -24,23 +24,20 @@ public class UtilsBot {
     }
 
     public static String getDiaSemana() {
+        
         return LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("pt"));
     }
 
-    private static String getPergunta(String textoMensagem) {
-        //regex !!!!!
-        return null;
+    public static String sendWeek() {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+        String dt_atual = formatter.format(new Date());
+        return "Hoje é " + LocalDate.now().getDayOfWeek().getDisplayName(TextStyle.FULL, new Locale("pt")) + ", " + dt_atual;
     }
-
 
     static SendMessage responseBot(Update update) {
         String textoMensagem = update.getMessage().getText().toLowerCase();
         String chatId = update.getMessage().getChatId().toString();
         String resposta = "";
-
-
-        String pergunta = getPergunta(textoMensagem)
-
 
         if (textoMensagem.startsWith("ola") ||
                 textoMensagem.startsWith("olá") ||
@@ -48,7 +45,7 @@ public class UtilsBot {
                 textoMensagem.startsWith("/start")) {
             resposta = "\u2601 Olá, eu sou a WENDY. Estou aqui para lhe auxiliar a entender o sentido da vida!\n"
                     +
-                    "Utilize um dos comandos:\nolá\ndata\nhora\nquem é você?";
+                    "Utilize um dos comandos:\nolá\ndata\nhora\nquem é você?\nque dia é hoje?";
 
         } else if ("data".equals(textoMensagem)) {
             resposta = "Hoje é " + getDiaSemana() + ", " + getData();
@@ -56,8 +53,10 @@ public class UtilsBot {
             resposta = getHora();
         } else if (textoMensagem.startsWith("quem é voc") || textoMensagem.startsWith("quem e voce")) {
             resposta = "\u2601 Eu sou a WENDY e sou um bot.";
+        } else if (textoMensagem.startsWith("que dia é hoje")) {
+            resposta = sendWeek();     
         } else if (textoMensagem.startsWith("/help")) {
-            resposta = "Utilize um dos comandos:\nolá\ndata\nhora\nquem é você?";
+            resposta = "Utilize um dos comandos:\nolá\ndata\nhora\nquem é você?\nque dia é hoje?";
         } else {
             resposta = "Não entendi!\nDigite /help para ver os comandos disponíveis.";
         }
